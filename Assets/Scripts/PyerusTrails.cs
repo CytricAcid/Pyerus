233d30c31ddf1960a62fb84e934cf38d1989b699
CytricAcid <beds7500@DNS.ILISLABS.EDU>
2016-05-17 10:34:56 -0500

    Wing Trails

---
+++ Assets/Scripts/PyerusTrails.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using AC;
+
+public class PyerusTrails : MonoBehaviour {
+
+	private Player Pyerus;
+	private TrailRenderer trail;
+
+
+	// Use this for initialization
+	void Start () {
+		Pyerus = KickStarter.player;
+		trail = GetComponent<TrailRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Pyerus.GetComponent<Player> ().isGliding == true && Pyerus.GetComponent<Player> ().GetMoveSpeed() > 0f)
+		{
+			trail.enabled = true;
+		}
+		else
+		{
+			trail.enabled = false;
+		}
+	}
+}
