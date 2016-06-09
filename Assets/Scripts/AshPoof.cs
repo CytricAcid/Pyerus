7d70da2dc096c5cabb6991c389f0c9fc74a672ef
CytricAcid <beds7500@DNS.ILISLABS.EDU>
2016-06-08 22:57:17 -0500

    Poofs and Lights
    nice

---
+++ Assets/Scripts/AshPoof.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AshPoof : MonoBehaviour {
+
+	private ParticleSystem self;
+	
+	// Use this for initialization
+	void Start () {
+		self = GetComponent<ParticleSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (self) {
+			if (!self.IsAlive ()) {
+				Destroy (self);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/JumpParticle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using AC;
+
+public class JumpParticle : MonoBehaviour {
+	private Player Player;
+	public ParticleSystem ashParticle;
+	public ParticleSystem newParticle;
+	
+
+	// Use this for initialization
+	void Start () {
+		Player = KickStarter.player;
+	}	
+
+	void OnTriggerEnter (Collider other)
+	{
+		newParticle = (ParticleSystem)Instantiate (ashParticle, Player.transform.position, transform.rotation);
+		newParticle.enableEmission = true;
+	}
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
