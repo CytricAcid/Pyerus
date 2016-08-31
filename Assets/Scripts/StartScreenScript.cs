fdddb86de83e9008aa5710d8898b04140a3eca16
CytricAcid <cyisawesome@gmail.com>
2016-08-31 16:23:07 -0500

    Title Screen
    yay

---
+++ Assets/Scripts/StartScreenScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreenScript : MonoBehaviour {
+
+	// Use this for initialization
+	public void GotoGame () {
+		Application.LoadLevel (0);
+	}
+	
+	// Update is called once per frame
+	public void GotoControlMenu () {
+		Application.LoadLevel (1);
+	}
+
+	public void GotoMainMenu () {
+		Application.LoadLevel (2);
+	}
+}
