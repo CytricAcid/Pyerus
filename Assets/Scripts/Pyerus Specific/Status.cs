3fd8507410a7f415bfea5d4badfc6c5abd0e1df0
CytricAcid <beds7500@DNS.ILISLABS.EDU>
2016-09-12 16:41:21 -0500

    Water is bad
    Dont submerge your bird in water, folks

--- Assets/Scripts/Pyerus Specific/Status.cs
+++ Assets/Scripts/Pyerus Specific/Status.cs
@@ -5,11 +5,57 @@ using AC;
 public class Status : MonoBehaviour {
 
 	private Player Pyerus;
+	public int currentHealth;
+	public int maxHealth;
+	public int displayHealth;
+	public bool inWater;
+	public float waterDamageDelay;
 
 	void Start ()
 	{
-		//Pyerus = GameObject.FindObjectOfType <Player> ();
+		currentHealth = maxHealth;	
 		Pyerus = KickStarter.player;
+		StartCoroutine (HealthRegen ());
+
 	}
-	// Use this for initialization
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Water") {
+			inWater = true;
+			StartCoroutine (WaterDamage ());
+		}
+	}
+	void OnTriggerExit (Collider other)
+	{
+		if (other.tag == "Water") {
+			inWater = false;
+			StartCoroutine (HealthRegen ());
+		}
+	}
+
+	IEnumerator WaterDamage ()
+	{
+		while (inWater == true) 
+		{
+			if (currentHealth > 0) {	
+				currentHealth--;
+			}
+			yield return new WaitForSeconds (waterDamageDelay);
+		}
+	}
+
+	IEnumerator HealthRegen ()
+	{	
+		while (inWater == false) 
+		{
+			yield return new WaitForSeconds (1);	
+			if (maxHealth > currentHealth) {	
+				currentHealth++;
+				if (currentHealth > maxHealth) {
+					currentHealth = maxHealth;
+				}
+			}
+		}
+	}
 }
